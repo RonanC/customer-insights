93dcc7cc57d2ade085b490a6a24da203ae173750
Ronan Connolly <hi@ronanconnolly.ie>
2015-11-08 15:08:32 +0000

    Created Node Web Service Moved website into views folder Added Heroku support

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res){
+  res.contentType('text/html');
+  res.status(200);
+  res.sendFile(path.join(__dirname + "views/index.html"));
+});
+
+app.listen(app.get('port'), function(){
+  console.log("Node web service is runnin on port ", app.get('port'));
+});
