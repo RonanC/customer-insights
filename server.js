28645e83e1e87d0f5a6645750fed711eeb03a42f
Ronan Connolly <hi@ronanconnolly.ie>
2015-11-16 17:46:11 +0000

    Reorganised file structure Used express app.use for the folders and paths JS and CSS now working with node server file

--- server.js
+++ server.js
@@ -5,10 +5,17 @@ var app = express();
 app.set('port', (process.env.PORT || 5050));
 app.set('view engine', 'jade');
 
+//  sets paths and path names for other folders
+app.use(express.static(path.join(__dirname, 'public')));
+app.use("/styles",  express.static(__dirname + '/public/styles'));
+app.use("/scripts", express.static(__dirname + '/public/js'));
+app.use("/images",  express.static(__dirname + '/public/images'));
+
 app.get('/', function(req, res){
   res.contentType('text/html');
   res.status(200);
-  res.sendFile(path.join(__dirname + "/views/index.html"));
+  res.sendFile(__dirname + "/public/index.html");
+  //console.log(__dirname);
 });
 
 app.listen(app.get('port'), function(){
